857f59f365c1c31c593202544f4043d175c373c6
Ekaterina <gaurung505@gmail.com>
2024-04-10 22:14:12 +0300

    jest

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -14,4 +14,19 @@ describe("Books names test suit", () => {
       "Гарри Поттер",
     ]);
   });
+  it("Empty array of books' names", () => {
+    const input = [];
+    const expected = [];
+    expect(sorting.sortByName(input)).toEqual(expected);
+  });
+  it("Books' names in descending order", () => {
+    const input = ["Властелин Колец", "Волшебник изумрудного города", "Гарри Поттер"];
+    const expected = ["Властелин Колец", "Волшебник изумрудного города", "Гарри Поттер"];
+    expect(sorting.sortByName(input)).toEqual(expected);
+  });
+  it("Should return 0 when names are equal", () => {
+    const input = ["Властелин Колец", "Властелин Колец"];
+    const expected = input;
+    expect(sorting.sortByName(input)).toEqual(expected); 
+  });
 });
